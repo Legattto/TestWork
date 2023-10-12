371f572984e9c8f707d99d408effc94dbf02b633
Андрей <57178547+Legattto@users.noreply.github.com>
2023-10-12 14:21:57 +0400

    Update Server.go

--- test/Server/Server.go
+++ test/Server/Server.go
@@ -1,44 +1,50 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"go.uber.org/fx"
-	"net"
-	"net/http"
+  "context"
+  "fmt"
+  "go.uber.org/fx"
+  "net"
+  "net/http"
 )
 
 func main() {
-	app := fx.New(
-		fx.Provide(
-			NewHTTPServer,
-		),
-		fx.Invoke(
-			func(*http.Server) {},
-		),
-	)
-	app.Run()
+  // Создаем новое приложение
+  app := fx.New(
+    fx.Provide(
+      NewHTTPServer, // Предоставляем зависимость - HTTP сервер
+    ),
+    fx.Invoke(
+      func(*http.Server) {}, // Вызываем функцию после создания HTTP сервера
+    ),
+  )
+  // Запускаем приложение
+  app.Run()
 }
 
+// Функция для создания HTTP сервера
 func NewHTTPServer(lc fx.Lifecycle) *http.Server {
-	srv := &http.Server{Addr: ":8080"}
-	srv.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			ln, err := net.Listen("tcp", srv.Addr)
-			if err != nil {
-				return err
-			}
-			fmt.Println("Starting HTTP server at", srv.Addr)
-			go srv.Serve(ln)
-			return nil
-		},
-		OnStop: func(ctx context.Context) error {
-			return srv.Shutdown(ctx)
-		},
-	})
-	return srv
+  // Создаем новый HTTP сервер на порту 8080
+  srv := &http.Server{Addr: ":8080"}
+  // Устанавливаем обработчик для HTTP запросов
+  srv.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
+  })
+  // Добавляем хук для жизненного цикла приложения
+  lc.Append(fx.Hook{
+    OnStart: func(ctx context.Context) error {
+      ln, err := net.Listen("tcp", srv.Addr)
+      if err != nil {
+        return err
+      }
+      fmt.Println("Starting HTTP server at", srv.Addr)
+      go srv.Serve(ln)
+      return nil
+    },
+    OnStop: func(ctx context.Context) error {
+      return srv.Shutdown(ctx)
+    },
+  })
+  return srv
 }
